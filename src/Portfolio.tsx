2e2392ec8e0f577bffdb4e10d43e77361552123e
JISHAN ANSARI <jishanbusiness099@gmail.com>
2026-01-09 04:09:53 +0530

    s

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,5 +1,5 @@
 import React, { useState, type FormEvent } from 'react';
-import './portfolio.css';
+import './Portfolio.css';
 
 const Portfolio: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>('skills');
